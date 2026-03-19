9953832444747af2d2644a92a49b643764755f78
Alok Kumar <alokbuilds.dev@gmail.com>
2026-03-19 14:28:11 +0530

    updated settings and urls

--- MediConnect/settings.py
+++ MediConnect/settings.py
@@ -138,3 +138,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Media files (user-uploaded content)
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
