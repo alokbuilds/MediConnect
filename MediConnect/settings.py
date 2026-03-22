5091759132a852128e40046974cbbc9096fef839
Alok Kumar <alokbuilds.dev@gmail.com>
2026-03-23 02:34:05 +0530

    updated CLOUDINARY_STORAGE and DEFAULT_FILE_STORAGE

--- MediConnect/settings.py
+++ MediConnect/settings.py
@@ -159,3 +159,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Media files (user-uploaded content)
 #MEDIA_URL = '/media/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
