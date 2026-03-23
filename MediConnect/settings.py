e1b47ac15c4c5dcf4396f8f33acf62e9babd0c2f
Alok Kumar <alokbuilds.dev@gmail.com>
2026-03-24 01:02:06 +0530

    fixed database config

--- MediConnect/settings.py
+++ MediConnect/settings.py
@@ -82,20 +82,20 @@ WSGI_APPLICATION = 'MediConnect.wsgi.application'
 # ========================
 # DATABASE (SQLite for now)
 # ========================
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+# DATABASES = {
+# 'default': {
+#     'ENGINE': 'django.db.backends.sqlite3',
+#     'NAME': BASE_DIR / 'db.sqlite3',
+# }
+# }
 
 # ========================
 # DATABASE (PostgreSQL for production)
 # ========================
 DATABASES = {
-    'default': dj_database_url.parse(
-        os.environ.get('DATABASE_URL')
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
     )
 }
 
