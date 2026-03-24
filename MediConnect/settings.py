bb7aee91ab8d95c4157ca1478eb162f4af2a4956
Alok Kumar <alokbuilds.dev@gmail.com>
2026-03-24 09:43:17 +0530

    fixed database config

--- MediConnect/settings.py
+++ MediConnect/settings.py
@@ -9,8 +9,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ========================
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
-DEBUG = True
-ALLOWED_HOSTS = ['*']
+# DEBUG = True
+# ALLOWED_HOSTS = ['*']
+
+DEBUG = False
+ALLOWED_HOSTS = ['mediconnect-xjx0.onrender.com']
+
 
 
 # ========================
