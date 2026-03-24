3be2a1c2536c8b85ffb2b5649fc4d7e86e4e421b
Alok Kumar <alokbuilds.dev@gmail.com>
2026-03-24 19:48:33 +0530

    add database dump

--- MediConnect/urls.py
+++ MediConnect/urls.py
@@ -19,9 +19,12 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from MediConnectApp.views import create_admin
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include("MediConnectApp.urls"))
+    path('',include("MediConnectApp.urls")),
+    path('create-admin/', create_admin)
 ]
 
 # if settings.DEBUG:
