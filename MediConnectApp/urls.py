2639397f5b6d20a4c84b87fa7aac05f17fe1a3c4
Alok Kumar <alokbuilds.dev@gmail.com>
2026-03-25 12:06:19 +0530

    changed role based login

--- MediConnectApp/urls.py
+++ MediConnectApp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from MediConnectApp import views #from MediConnectApp import views
+from . import views #from MediConnectApp import views
 
 urlpatterns = [    
     path('', views.home, name="home"),
