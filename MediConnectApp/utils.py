e7c50e4fdeb12e469cfb5834827f505b330eea91
Alok Kumar <alokbuilds.dev@gmail.com>
2026-03-27 04:51:51 +0530

    database setting updated

--- MediConnectApp/utils.py
+++ MediConnectApp/utils.py
@@ -1,14 +1,40 @@
+# def get_user_role(user):
+#     if user.is_superuser:
+#         return "django_admin"
+
+#     if hasattr(user, "hospitaladminprofile"):
+#         return "hospital_admin"
+
+#     if hasattr(user, "doctorprofile"):
+#         return "doctor"
+
+#     if hasattr(user, "patientprofile"):
+#         return "patient"
+
+#     return None
+
+# utils.py
+
 def get_user_role(user):
     if user.is_superuser:
         return "django_admin"
 
-    if hasattr(user, "hospitaladminprofile"):
+    try:
+        user.hospitaladminprofile
         return "hospital_admin"
+    except:
+        pass
 
-    if hasattr(user, "doctorprofile"):
+    try:
+        user.doctorprofile
         return "doctor"
+    except:
+        pass
 
-    if hasattr(user, "patientprofile"):
+    try:
+        user.patientprofile
         return "patient"
+    except:
+        pass
 
     return None
