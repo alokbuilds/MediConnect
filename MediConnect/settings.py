c9c9db85fbed9f66df2612e3f6e6343e094830f8
Alok Kumar <alokbuilds.dev@gmail.com>
2026-03-28 19:04:58 +0530

    add ssl for postgres

--- MediConnect/settings.py
+++ MediConnect/settings.py
@@ -84,7 +84,8 @@ if os.environ.get("DATABASE_URL"):
     DATABASES = {
         'default': dj_database_url.config(
             default=os.environ.get("DATABASE_URL"),
-            conn_max_age=600
+            conn_max_age=600,
+            ssl_require=True
         )
     }
 else:
