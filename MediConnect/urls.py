7925c3adc4e37784e89e9543019313ae4970b4ad
Alok Kumar <alokbuilds.dev@gmail.com>
2026-07-06 20:06:45 +0530

    DATABASE_URL enabled

--- MediConnect/urls.py
+++ MediConnect/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    # App URLs
+    # MediConnectApp URLs
     path('', include('MediConnectApp.urls')),
 ]
 
